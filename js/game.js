846b42fd662b5cf05e91753827404c51eae4d28d
Michał Różański <rozanski.michal.mr@gmail.com>
2018-10-29 21:25:55 +0100

    Redefined physics, added jump to hero movements

--- js/game.js
+++ js/game.js
@@ -66,39 +66,38 @@ class Swing {
 
 
 class Animation {
-    constructor(frames, filePrefix, fileSuffix) {
-        this.directory = "img/game";
-        this.filePrefix = filePrefix;
-        this.fileSuffix = fileSuffix;
-        this.file = `${this.directory}/${this.filePrefix}000.${this.fileSuffix}`;
+    constructor(frames, animationClass, framesPerRow, frameWidth, frameHeight) {
+        this.animationClass = animationClass;
         this.frames = frames;
+        this.framesPerRow = framesPerRow;
         this.currentFrame = 0;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.posX = 0;
+        this.posY = 0;
 
-        this.getFrameString = () => {
-            const countString = `00${this.currentFrame}`;
-            return countString.substr(countString.length - 3);
+        this.setFramePosition = () => {
+            const currentRow = Math.floor(this.currentFrame / this.framesPerRow);
+            const currentFrame = this.currentFrame % this.framesPerRow;
+            this.posX = currentFrame / (this.framesPerRow - 1) * 100;
+            this.posY = 100 * currentRow / Math.floor((this.frames) / (this.framesPerRow));
         }
 
         this.updateFrame = () => {
             this.currentFrame = (this.currentFrame + 1) % (this.frames + 1);
-            this.file = `${this.directory}/${this.filePrefix}${this.getFrameString()}.${this.fileSuffix}`;
+            this.setFramePosition();
         }
     }
 }
 
-
-
-
-
-
 const gameSettings = {
     setDimensions: function () {
         this.dimensions = getGameWidthHeight();
+        this.dimensions.noSwimLines = 2;
     },
     dimensions: {
         width: 0,
-        height: 0
-
+        height: 0,
     },
     gameStatus: "stoped"
 };
@@ -140,69 +139,110 @@ const enemyTypes = [{
     }
 ];
 
+const jump = {
+    gravityAsc: 0.5,
+    gravityDesc: 0.6,
+    gravityClimax: 0.2,
+    initialVelocity: -16,
+}
+
 let enemies = [];
-const initialScore = 30;
+const initialHealth = 30;
 const fps = 60;
 let interval;
 let timeInterval = 0;
 let time = Date.now();
-let noSwimLines = 2;
 let level = 3;
-let upPressed = false;
-let downPressed = false;
 let gameOver = false;
 let backgroundPosition = 0;
 
-
 const hero = {
     x: 0,
-    swLine: 1,
-    height: 175,
+    y: 320,
+    swLine: 2,
+    height: 170,
     width: 100,
     moved: false,
-    score: initialScore,
+    health: initialHealth,
     state: "run",
-    animRun: new Animation(42, "run/r_", "png"),
-    animJump: new Animation(24, "jump/j_", "png"),
-    currentAnim: null
-}
+    animRun: new Animation(42, "animate__hero--run", 9, 100, 169),
+    animJump: new Animation(24, "animate__hero--jump", 5, 83, 168),
+
+    jumpInitialY: 0,
+    velocity: 0,
+    minY: 1000,
 
 
+    setCurrentAnim: function () {
+        if (this.state === "run") {
+            this.currentAnim = this.animRun;
+        } else {
+            this.currentAnim = this.animJump;
+        }
+    },
 
-function setCurrentAnimHero() {
-    if (hero.state === "run") {
-        hero.currentAnim = hero.animRun;
-    } else {
-        hero.currentAnim = hero.animJump;
+    changeState: function (state) {
+        if (this.state !== "jump" && state === "jump") {
+            this.initJump();
+        }
+        this.state = state;
+        this.setCurrentAnim();
+    },
+
+    initJump: function () {
+        this.velocity = jump.initialVelocity;
+        this.jumpInitialY = this.y;
+    },
+
+    finishJump: function () {
+        this.initialVelocity = 0;
+        this.y = this.jumpInitialY;
+    },
+
+    jumpHero: function () {
+        if (this.state === "jump") {
+            this.y += this.velocity;
+            this.minY = this.minY < this.y ? this.minY : this.y
+            if (this.velocity < 0) {
+                this.velocity += jump.gravityAsc;
+            } else if (Math.abs(this.velocity) < 0.2 * Math.abs(jump.initialVelocity)) {
+                this.velocity += jump.gravityClimax;
+            } else {
+                this.velocity += jump.gravityDesc;
+            }
+            if (this.jumpInitialY <= this.y) {
+                this.changeState("run");
+                this.finishJump();
+            }
+        }
+        console.log(this.y, this.velocity);
+    },
+
+    moveHero: function () {
+        if (hero.state === "jump") {
+            hero.jumpHero();
+        }
+    },
+
+    setY: function () {
+        const dimensions = gameSettings.dimensions;
+        const adjustPosition = -dimensions.height / dimensions.noSwimLines;
+        this.y = Math.floor(dimensions.height / dimensions.noSwimLines * this.swLine + adjustPosition);
+    },
+
+    setX: function () {
+        this.x = Math.floor(gameSettings.dimensions.width / 6);
     }
 }
 
-function animateHero() {
-    setCurrentAnimHero();
-    hero.currentAnim.updateFrame();
-    const elHero = document.getElementById("game__hero");
-    elHero.style.background = `url(${hero.currentAnim.file}) no-repeat`;
-}
 
-function preloadAnimations() {
-    let frames = hero.animRun.frames;
-    const elHero = document.getElementById("game__hero");
-    for (i = 0; i < frames; i++) {
-        hero.animRun.updateFrame();
-        elHero.style.background = `url(${hero.animRun.file}) no-repeat`;
-    }
-
-    frames = hero.animJump.frames;
-    for (i = 0; i < frames; i++) {
-        hero.animRun.updateFrame();
-        elHero.style.background = `url(${hero.animJump.file}) no-repeat`;
-    }
-}
 
 function appendHero() {
-    hero.x = gameSettings.dimensions.width / 6;
+    hero.setX();
+    //hero.setY();
+    hero.changeState(hero.state);
     const element = document.createElement("div");
-    element.classList.add("animate__hero");
+    element.classList.add("animate__hero", hero.currentAnim.animationClass);
     element.setAttribute("id", "game__hero");
     gameContainer.appendChild(element);
 }
@@ -216,7 +256,6 @@ function calculateIntervals() {
 function resetIntervals() {
     timeInterval = 0;
 }
-
 
 function moveEnemies() {
     for (let enemy of enemies) {
@@ -232,12 +271,12 @@ function randomizeEnemyType() {
 }
 
 function randomizeSwimLine() {
-    const sw = Math.floor(Math.random() * noSwimLines) + 1;
+    const sw = Math.floor(Math.random() * gameSettings.dimensions.noSwimLines) + 1;
     return sw;
 }
 
 function chooseEnemySwimLine() {
-    const swNumber = randomizeSwimLine() / noSwimLines;
+    const swNumber = randomizeSwimLine() / gameSettings.dimensions.noSwimLines;
     return swNumber;
 }
 
@@ -245,7 +284,7 @@ function createNewEnemy() {
     if (timeInterval >= 5000 * (60 / fps) / level) {
         const dimensions = gameSettings.dimensions;
         const x = dimensions.width;
-        const adjustPosition = -3 / 4 * dimensions.height / noSwimLines;
+        const adjustPosition = -3 / 4 * dimensions.height / dimensions.noSwimLines;
         const y = Math.floor(dimensions.height * chooseEnemySwimLine()) + adjustPosition;
         const newEnemy = new Enemy(x, y);
         newEnemy.enemyType = enemyTypes[randomizeEnemyType()];
@@ -281,36 +320,12 @@ function removeHero() {
     }
 }
 
-function heroY() {
-    const dimensions = gameSettings.dimensions;
-    const adjustPosition = -dimensions.height / noSwimLines;
-    const heroy = Math.floor(dimensions.height / noSwimLines * hero.swLine + adjustPosition);
-    return heroy;
-}
 
-function moveHero() {
-    if (!hero.moved) {
-        if (downPressed) {
-            downPressed = false;
-            if (hero.swLine < noSwimLines) hero.swLine++;
-            hero.moved = true;
-        }
-        if (upPressed) {
-            upPressed = false;
-            if (hero.swLine > 1) hero.swLine--;
-            hero.moved = true;
-        }
-    }
-    if (!upPressed && !downPressed) {
-        hero.moved = false;
-    }
-}
 
 function checkCollisions() {
-    const heroy = heroY();
     for (let enemy of enemies) {
         if (enemy.x > hero.x + hero.width / 4 && enemy.x < hero.x + hero.width * 3 / 4 &&
-            enemy.y < heroy + hero.height && enemy.y > heroy) {
+            enemy.y < hero.y + hero.height && enemy.y > hero.y) {
             changeScore(enemy);
             removeEnemy(enemy);
             enemies.splice(enemies.indexOf(enemy), 1);
@@ -321,10 +336,9 @@ function checkCollisions() {
 }
 
 function changeScore(enemy) {
-    //hero.score += enemy.enemyType.score;
-    //console.log(hero.score);
+    hero.health += enemy.enemyType.score;
 
-    if (hero.score <= 0) {
+    if (hero.health <= 0) {
         //alert("Game Over!!!!");
         //clearInterval(interval);
         //gameOver = true;
@@ -336,8 +350,10 @@ function checkBoundries() {
         if (enemies[i].x < 0) {
             removeEnemy(enemies[i]);
             enemies.splice(i, 1);
+            return true;
         }
     }
+    return false;
 }
 
 function removeEnemy(enemy) {
@@ -358,10 +374,12 @@ function getGameWidthHeight() {
 
 function drawHero() {
     appendHero();
-    animateHero();
+    hero.currentAnim.updateFrame();
     const element = document.getElementById("game__hero");
-    element.style.left = hero.x + 'px';
-    element.style.top = heroY() + 'px';
+    element.style.transform = `translate(${hero.x}px, ${hero.y}px)`;
+    element.style.backgroundPosition = `${hero.currentAnim.posX}% ${hero.currentAnim.posY}%`
+    element.style.width = `${hero.currentAnim.frameWidth}px`;
+    element.style.height = `${hero.currentAnim.frameHeight}px`;
 }
 
 function setStyleEnemy(enemy) {
@@ -379,14 +397,12 @@ function drawEnemies() {
 function clearBoard() {
     removeEnemies();
     removeHero();
-
 }
 
 function resetGame() {
     if (gameSettings.gameStatus === 'start') {
         removeEnemies();
         enemies = [];
-        hero.swLine = 1;
         clearInterval(interval);
         setGameInterval();
         gameOver = false;
@@ -404,7 +420,7 @@ function frame() {
     animateBackground();
     createNewEnemy();
     moveEnemies();
-    moveHero();
+    hero.moveHero();
     checkBoundries();
     if (checkCollisions()) {
         gulp("snd/gulp.mp3");
@@ -421,10 +437,8 @@ function gulp(filename) {
 }
 
 function keyPressHandler(e) {
-    if (e.keyCode == 38) {
-        upPressed = true;
-    } else if (e.keyCode == 40) {
-        downPressed = true;
+    if (e.keyCode === 32) {
+        hero.changeState("jump");
     }
 }
 
@@ -448,8 +462,7 @@ function game() {
     if (gameSettings.gameStatus === 'stoped') {
         gameSettings.gameStatus = 'start';
         clearInterval(interval);
-        appendHero();
-        preloadAnimations();
+        //appendHero();
         document.addEventListener("keydown", keyPressHandler, false);
         setGameInterval();
     }
