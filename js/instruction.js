4c6be224a7d972e69943d4c6b1fe84b595139a21
delete.cookies <anetajankowska@o2.pl>
2018-11-21 14:06:18 +0100

    dodanie instrukcji, zakomentowane zmiany w game.html oraz próby z instruction.js - jak nie bedzie czasu, to te pliki wywalę

---
+++ js/instruction.js
@@ -0,0 +1,24 @@
+/*const elemList = document.getElementsByClassName("c-box__rules_of_game");
+    elemList[0].addEventListener("click", function () { setNthBox(0); });
+
+
+const boxList = document.getElementsByClassName("c-flexbox-instruction_box");
+const arrowList = document.getElementsByClassName("c-flexbox-instruction__arrow");
+arrowList[0].addEventListener("click", function () { clearBox() });
+
+window.onload = clearBox();
+
+function clearBox() {
+    for (let i = 0; i < boxList.length; i++) {
+        boxList[i].style.display = "none";
+    }
+}
+
+function setNthBox(value) {
+
+    clearBox();
+
+    boxList[value].style.display = "flex";
+}
+*/
+
